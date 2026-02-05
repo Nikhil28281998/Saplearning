74dc81c8bcc199bc114caef24040d8115599eeb2
Nikhil28281998 <kumarnikihl2828@gmail.com>
2026-02-05 15:05:25 -0500

    Fix server.js to use correct CDS bootstrap hook

--- srv/server.js
+++ srv/server.js
@@ -1,19 +1,22 @@
 const cds = require('@sap/cds');
+const cors = require('cors');
 
-module.exports = async (srv) => {
+// CDS server customization
+cds.on('bootstrap', (app) => {
   // Enable CORS for local development
-  const cors = require('cors');
-  srv.on('bootstrap', (app) => {
-    app.use(cors());
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-      } else {
-        next();
-      }
-    });
+  app.use(cors());
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
   });
-};
+});
+
+module.exports = cds.server;
